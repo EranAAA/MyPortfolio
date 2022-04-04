43dadc3baa90f82a7121101608884b84ddfe6a88
EranAAA <eranavichzer@gmail.com>
2022-04-04 18:02:55 +0300

    Fix img bug

--- js/proj-service.js
+++ js/proj-service.js
@@ -18,7 +18,7 @@ function _createProject(id, name, title, desc, url, labels) {
 
 function _createProjects() {
     gProjs = [
-        _createProject('Minesweeper', 'Minesweeper', 'Sprint 1', 'The old-Update Minesweeper', 'https://eranaaa.github.io/MineSweeper/', ['Matrixes', 'keyboard events']),
+        _createProject('MineSweeper', 'Minesweeper', 'Sprint 1', 'The old-Update Minesweeper', 'https://eranaaa.github.io/MineSweeper/', ['Matrixes', 'keyboard events']),
         _createProject('BookShop', 'Books Shop', 'Exercise', 'better then monday.com', 'https://eranaaa.github.io/BookShop/', ['Matrixes', 'keyboard events'])
     ]
 }
