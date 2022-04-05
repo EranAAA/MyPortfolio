b6f1ce94ca6ab8e783ae628f16e41e9b01de5782
EranAAA <eranavichzer@gmail.com>
2022-04-05 07:10:57 +0300

    Added more projects

--- js/proj-service.js
+++ js/proj-service.js
@@ -19,7 +19,10 @@ function _createProject(id, name, title, desc, url, labels) {
 function _createProjects() {
     gProjs = [
         _createProject('MineSweeper', 'Minesweeper', 'Sprint 1', 'The old-Update Minesweeper', 'https://eranaaa.github.io/MineSweeper/', ['Matrixes', 'keyboard events']),
-        _createProject('BookShop', 'Books Shop', 'Exercise', 'better then monday.com', 'https://eranaaa.github.io/BookShop/', ['Matrixes', 'keyboard events'])
+        _createProject('BookShop', 'Books Shop', 'Exercise', 'better then nothing', 'https://eranaaa.github.io/BookShop/', ['Matrixes', 'keyboard events']),
+        _createProject('PacMan', 'PacMan', 'Exercise', 'check in Google', 'https://eranaaa.github.io/PacMAn/', ['Matrixes', 'keyboard events']),
+        _createProject('BallBoard', 'Ball Board', 'Exercise', 'check in Google', 'https://eranaaa.github.io/ballBoard/', ['Matrixes', 'keyboard events']),
+        _createProject('TouchTheNums', 'Touch The Nums', 'Exercise', 'check in Google', 'https://eranaaa.github.io/TouchTheNums/', ['Matrixes', 'keyboard events'])
     ]
 }
 
